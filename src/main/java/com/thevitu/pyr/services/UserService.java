7c68010f2d8638ecdc7f58839952141de10d78eb
Vitor Fiurilo <thevitu@gmail.com>
2020-09-20 13:40:39 +0100

    config recruit hero endpoint

--- src/main/java/com/thevitu/pyr/services/UserService.java
+++ src/main/java/com/thevitu/pyr/services/UserService.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thevitu.pyr.entities.Hero;
@@ -46,8 +48,8 @@ public class UserService {
 		return this.userRepository.save(user);
 	}
 
-	
-	public Hero recruitHero(Long userId) {
+	@GetMapping("/api/recruitHero")
+	public Hero recruitHero(@RequestParam Long userId) {
 		Hero hero = this.heroService.getRandomHero();
 		User user = this.userRepository.getOne(userId);
 		if (user.getRecruitmentPosters() < 1) {
